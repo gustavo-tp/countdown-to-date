5bd0b69b3d5e279d48817f1d73e6e31f4c53f1c2
gustavo-tp <gustavo1190.gt@gmail.com>
2020-01-05 00:09:59 -0300

    Template to show countdown to a date

---
+++ script.js
@@ -0,0 +1,30 @@
+const countDate = new Date('Jan 20, 2020 00:00:00').getTime();
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+const cards = {
+    day: document.getElementById('day'),
+    hour: document.getElementById('hour'),
+    minute: document.getElementById('minute'),
+    second: document.getElementById('second')
+}
+
+function newYear() {
+    let now = new Date().getTime();
+    let gap = countDate - now;
+
+    let d = Math.floor(gap / day);
+    let h = Math.floor((gap % day) / hour);
+    let m = Math.floor((gap % hour) / minute);
+    let s = Math.floor((gap % minute) / second);
+
+    cards.day.innerHTML = d;
+    cards.hour.innerHTML = h;
+    cards.minute.innerHTML = m;
+    cards.second.innerHTML = s;
+}
+
+setInterval(newYear, 1000);
